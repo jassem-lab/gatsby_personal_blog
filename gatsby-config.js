4aef35d6027f65e472a64f09f3c74f0df963f4b0
Jassem Gaaloul <jassemgaaloul123@gmail.com>
2021-01-02 02:43:45 +0100

    gatsby Personal Blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Jas Coding Blog`,
+    description: `My personal blog. Sharing all about development and tech.`,
+    author: `@JassemDev`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
